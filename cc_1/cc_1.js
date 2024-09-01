7998861ac1a5d709fdb919a22115cc4838b62e82
kyle <kyle.offenbacher@gmail.com>
2024-09-01 19:55:40 -0400

    Task 1 - Employee Information

---
+++ cc_1/cc_1.js
@@ -0,0 +1,14 @@
+//cc_1 project
+
+let employeeName = "Company Name";
+const employeeID = 1234;
+var isActive = true;
+
+console.log("Employee Name:", employeeName);
+console.log("Type of employeeName:", typeof employeeName);
+
+console.log("Employee ID:", employeeID);
+console.log("Type of employeeID:", typeof employeeID);
+
+console.log("Is Active:", isActive);
+console.log("Type of isActive:", typeof isActive);
