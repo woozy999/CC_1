05ad7ff1d48183f77a66a015d6b274a87e377daf
kyle <kyle.offenbacher@gmail.com>
2024-09-01 20:44:31 -0400

    Task 3 - Financial Calculations

--- cc_1/cc_1.js
+++ cc_1/cc_1.js
@@ -23,3 +23,22 @@ const productDetails = { name: "Computer", price: 100.00, inStock: true };
 console.log("Products:", products);
 console.log("Product Details:", productDetails);
 
+//part 3
+
+let accountBalance = 9900.00;
+console.log("Initial Balance:", accountBalance);
+
+accountBalance += 10;
+console.log("New balance after deposit:", accountBalance);
+
+accountBalance -= 20;
+console.log("New balance after withdrawal:", accountBalance);
+
+accountBalance *= 1.10;
+console.log("New balance after 10% interest:", accountBalance);
+
+accountBalance /= 1.15;
+console.log("New balance after 15% fee:", accountBalance);
+
+accountBalance %= 100;
+console.log("Remainder after dividing by 100:", accountBalance);
